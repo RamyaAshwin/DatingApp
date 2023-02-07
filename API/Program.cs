4f1dd119c296108966383408f7b36185018de57c
RamyaAshwin <ramyasatheesh85@gmail.com>
2023-02-07 12:35:33 +0530

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,11 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddDbContext<DataContext>(opt => 
 {
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
